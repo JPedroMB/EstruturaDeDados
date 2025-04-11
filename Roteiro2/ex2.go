6cda63d2d7460028c8521dc44baeaa033a91c869
JPedroMB <166443434+JPedroMB@users.noreply.github.com>
2025-04-11 11:15:48 -0300

    Create ex2.go

---
+++ Roteiro2/ex2.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var ra [7]int
+	ra[1] = 1
+	ra[2] = 2
+	ra[3] = 4
+	ra[4] = 3
+	ra[5] = 3
+	ra[6] = 1
+	fmt.Println(ra[1], ra[2], ra[3], ra[4], ra[5], ra[6])
+
+}
